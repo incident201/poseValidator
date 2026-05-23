0ce76896f495939ba5e62b47cd7e738d24d21c29
incident201 <58225046+incident201@users.noreply.github.com>
2026-05-23 20:39:47 +0300

    Remove KSP and Moshi codegen from build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,8 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
-  alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
-}
-
-kotlin {
-  jvmToolchain(21)
 }
 
 android {
@@ -123,6 +118,4 @@ dependencies {
   androidTestImplementation(libs.androidx.runner)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
-  "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
 }
