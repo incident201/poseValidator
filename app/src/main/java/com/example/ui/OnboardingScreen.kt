56cf9f7bfe712e0127825bf13221911df5d1326f
incident201 <58225046+incident201@users.noreply.github.com>
2026-06-03 10:29:57 +0300

    Fix onboarding scoped modifier imports

--- app/src/main/java/com/example/ui/OnboardingScreen.kt
+++ app/src/main/java/com/example/ui/OnboardingScreen.kt
@@ -15,12 +15,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.HorizontalPager
