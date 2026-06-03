429fd6c7e4cc97a66ea4f7ae56957bb9188d07a6
incident201 <58225046+incident201@users.noreply.github.com>
2026-06-03 20:15:00 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 
   buildTypes {
     release {
-      isMinifyEnabled = true
+      isMinifyEnabled = false
       isShrinkResources = false
       isCrunchPngs = true
       proguardFiles(
