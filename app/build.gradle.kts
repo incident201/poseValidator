380cb3436fd18311e489191e456aa5b79e3f09c8
incident201 <58225046+incident201@users.noreply.github.com>
2026-06-17 08:44:18 +0300

    Raise compile SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
   namespace = "com.incident201.poseguard"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 37
 
   defaultConfig {
     applicationId = "com.incident201.poseguard"
