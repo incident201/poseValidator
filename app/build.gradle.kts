b801fa0821414f1e84537dce4c48e9d400e3d6cb
incident201 <58225046+incident201@users.noreply.github.com>
2026-06-22 15:10:36 +0300

    Disable minification and resource shrinking for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
 
   buildTypes {
     release {
-      isMinifyEnabled = true
-      isShrinkResources = true
+      isMinifyEnabled = false
+      isShrinkResources = false
       isCrunchPngs = true
       proguardFiles(
         getDefaultProguardFile("proguard-android-optimize.txt"),
